f04be4ef312ec6485c751909ae98d03f89ffd961
Ankit Agarwal <aagarwal_601@berkeley.edu>
2021-04-26 22:03:46 -0700

    start lightning structure

---
+++ Assets/Scripts/Lightning/LightningMaster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningMaster : MonoBehaviour
+{
+    const string headerDecoration = " --- ";
+    [Header(headerDecoration + "Main" + headerDecoration)]
+
+    // ignored for now, just try to generate one single lightning
+    float lightningDensity;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject initialSegment = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        initialSegment.transform.position = new Vector3(-2, 1, 0);
+        initialSegment.GetComponent<MeshRenderer>().material = Resources.Load("Materials/LightningMaterial.mat") as Material;
+        // LightningSegment root = new LightningSegment();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Lightning/LightningSegment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// lightning prefab
+
+public class LightningSegment : MonoBehaviour
+{
+    // Parameters
+    float length;
+    Vector3 ptA;
+    Vector3 ptB;
+
+    // constructor
+    public LightningSegment(Vector3 startPt, float length, Vector3 direction)
+    {
+        ptA = startPt;
+        // ptB = startPt + length * direction.Normalized();
+    }
+}
+
