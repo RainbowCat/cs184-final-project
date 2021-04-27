84f176d23d44d9f1bfa3e294c3f8de85a1612022
Ankit Agarwal <aagarwal_601@berkeley.edu>
2021-04-26 22:18:16 -0700

    updated lightning stuff

--- Assets/Scripts/Lightning/LightningMaster.cs
+++ Assets/Scripts/Lightning/LightningMaster.cs
@@ -10,15 +10,17 @@ public class LightningMaster : MonoBehaviour
     // ignored for now, just try to generate one single lightning
     float lightningDensity;
 
-
+    public Material lightningMaterial;
 
     // Start is called before the first frame update
     void Start()
     {
         GameObject initialSegment = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+
         initialSegment.transform.position = new Vector3(-2, 1, 0);
-        initialSegment.GetComponent<MeshRenderer>().material = Resources.Load("Materials/LightningMaterial.mat") as Material;
-        // LightningSegment root = new LightningSegment();
+        initialSegment.transform.localScale = new Vector3(0.1f, 5, 0.1f);
+        initialSegment.GetComponent<MeshRenderer>().material = lightningMaterial;
+        // LightningSegment root = new LightningSegment(); 
     }
 
     // Update is called once per frame
