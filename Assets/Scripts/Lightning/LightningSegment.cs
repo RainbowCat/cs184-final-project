54d259d8e66651bb7ae278e88a37b8932b7982c0
Ankit Agarwal <aagarwal_601@berkeley.edu>
2021-04-26 22:54:12 -0700

    lightning segment

--- Assets/Scripts/Lightning/LightningSegment.cs
+++ Assets/Scripts/Lightning/LightningSegment.cs
@@ -7,15 +7,24 @@ using UnityEngine;
 public class LightningSegment : MonoBehaviour
 {
     // Parameters
-    float length;
-    Vector3 ptA;
-    Vector3 ptB;
+    public float length;
+    public Vector3 start;
+    public Vector3 direction;
 
-    // constructor
-    public LightningSegment(Vector3 startPt, float length, Vector3 direction)
+    public Material lightningMaterial;
+
+    void Start()
     {
-        ptA = startPt;
-        // ptB = startPt + length * direction.Normalized();
+        createSegment(start, length, direction);
+    }
+
+    public void createSegment(Vector3 startPt, float length, Vector3 direction)
+    {
+        GameObject initialSegment = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        initialSegment.transform.position = start + direction * length / 2;
+        initialSegment.transform.localScale = new Vector3(0.1f, length, 0.1f);
+        initialSegment.transform.rotation.SetFromToRotation(new Vector3(0, 1, 0), direction);
+        initialSegment.GetComponent<MeshRenderer>().material = lightningMaterial;
     }
 }
 
